364a6066a4b5c1a2929a0538ade518a8c865b349
Adrian Richter <adrian.richter@euvic.pl>
2019-12-16 08:06:40 +0100

    commit5

--- functions/FunctionDescriptor.cs
+++ functions/FunctionDescriptor.cs
@@ -11,63 +11,6 @@ namespace CodeFirstStoreFunctions
     {
 
 
-        public FunctionDescriptor(string name, IEnumerable<ParameterDescriptor> parameters,
-            EdmType[] returnTypes, string resultColumnName, string databaseSchema, StoreFunctionKind storeFunctionKind, bool? isBuiltIn, bool? isNiladic)
-      {
-            Debug.Assert(!string.IsNullOrWhiteSpace(name), "invalid name");
-            Debug.Assert(parameters != null, "parameters is null");
-            Debug.Assert(parameters.All(p => p.EdmType != null), "invalid parameter type");
-            Debug.Assert(returnTypes != null && returnTypes.Length > 0, "returnTypes array is null or empty");
-            Debug.Assert(storeFunctionKind == StoreFunctionKind.StoredProcedure|| returnTypes.Length == 1, "multiple return types for non-sproc");
-
-            _name = name;
-            _returnTypes = returnTypes;
-            _parameters = parameters.ToArray();
-            _resultColumnName = resultColumnName;
-            _databaseSchema = databaseSchema;
-            _storeFunctionKind = storeFunctionKind;
-            _isBuiltIn = isBuiltIn;
-            _isNiladic = isNiladic;
-      }
-
-        public string Name
-        {
-            get { return _name; }
-        }
-
-        public EdmType[] ReturnTypes
-        {
-            get { return _returnTypes; }
-        }
-
-        public IEnumerable<ParameterDescriptor> Parameters
-        {
-            get { return _parameters; }
-        }
-
-        public string ResultColumnName
-        {
-            get { return _resultColumnName; }
-        }
-
-        public string DatabaseSchema
-        {
-            get { return _databaseSchema; }
-        }
-
-        public StoreFunctionKind StoreFunctionKind
-        {
-            get { return _storeFunctionKind; }
-        }
-
-        public bool? IsBuiltIn
-        {
-            get { return _isBuiltIn; }
-        }
-
-        public bool? IsNiladic
-        {
-            get { return _isNiladic; }
-        }
+      
     }
 }
